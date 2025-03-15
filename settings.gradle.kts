309176dc0fdceba342ecd7e64b226ea0fc3cf555
NandaYolanda06 <156060236+NandaYolanda06@users.noreply.github.com>
2025-03-15 13:15:43 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Firebase_Kelompok3_Praktikum9_PPLG2"
+include(":app")
+ 
